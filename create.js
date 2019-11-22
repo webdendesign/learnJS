9f315bdac4d8c515e7386eb4f0bc56bd1891806c
Den <fabri.din@yandex.ru>
2019-11-22 16:54:23 +0500

    begin

---
+++ create.js
@@ -0,0 +1,72 @@
+
+
+// Б20. Все файлы JS представляют собой отдельные модули в IIFE
+// Экспорт значений производится через глобальную область видимости.
+//  Код вне модуля запрещён. Вне модуля могут располагаться комментарии и утилитные инструкции,
+//   такие как 'use strict';
+
+// Пример правильного модуля:
+
+'use strict';
+
+(function () {
+  window.load = function (url, onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onLoad);
+
+    xhr.responseType = 'json';
+    xhr.open('GET', url);
+    xhr.send();
+  };
+})();
+
+// Б21. Все значения, используемые только внутри модулей ограничены по видимости
+// Из модуля ничего не должно попадать случайными образом в глобальную область видимости
+
+// Неправильно:
+
+'use strict';
+
+var ENTER_KEYCODE = 13;
+
+(function () {
+
+  var userIcon = document.querySelector('.user');
+
+  userIcon.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      popup.classList.remove('hidden');
+    }
+  });
+})();
+
+// Правильно:
+
+'use strict';
+
+(function () {
+  var ENTER_KEYCODE = 13;
+
+  var userIcon = document.querySelector('.user');
+
+  userIcon.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      popup.classList.remove('hidden');
+    }
+  });
+})();
+
+Б22. Код является кроссбраузерным и не вызывает ошибок в разных браузерах и разных операционных системах
+При проверке этого критерия, необходимо удостовериться в правильной работе и отсутствии сообщений об ошибках в выполняемых скриптах только в последних версиях браузеров: Chrome, Firefox, Safari, Microsoft Edge.
+
+Б23. Нельзя пользоваться глобальной переменной event
+Приводит к неосознанному коду:
+
+var elem = document.querySelector('.test');
+
+var onElemClick = function () {
+  event.target.innerText = 'you really need event';
+};
+
+elem.addEventListener('click', oneElemClick);
+
