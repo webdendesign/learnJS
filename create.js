0b0c43c01a40b91090cd9edbaaee3206121d8c49
Den <fabri.din@yandex.ru>
2019-11-23 19:55:48 +0500

    add base

--- create.js
+++ create.js
@@ -1,5 +1,3 @@
-
-
 // Б20. Все файлы JS представляют собой отдельные модули в IIFE
 // Экспорт значений производится через глобальную область видимости.
 //  Код вне модуля запрещён. Вне модуля могут располагаться комментарии и утилитные инструкции,
@@ -7,15 +5,15 @@
 
 // Пример правильного модуля:
 
-'use strict';
+"use strict";
 
-(function () {
-  window.load = function (url, onLoad) {
+(function() {
+  window.load = function(url, onLoad) {
     var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', onLoad);
+    xhr.addEventListener("load", onLoad);
 
-    xhr.responseType = 'json';
-    xhr.open('GET', url);
+    xhr.responseType = "json";
+    xhr.open("GET", url);
     xhr.send();
   };
 })();
@@ -25,48 +23,61 @@
 
 // Неправильно:
 
-'use strict';
+("use strict");
 
 var ENTER_KEYCODE = 13;
 
-(function () {
+(function() {
+  var userIcon = document.querySelector(".user");
 
-  var userIcon = document.querySelector('.user');
-
-  userIcon.addEventListener('keydown', function (evt) {
+  userIcon.addEventListener("keydown", function(evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
-      popup.classList.remove('hidden');
+      popup.classList.remove("hidden");
     }
   });
 })();
 
 // Правильно:
 
-'use strict';
+("use strict");
 
-(function () {
+(function() {
   var ENTER_KEYCODE = 13;
 
-  var userIcon = document.querySelector('.user');
+  var userIcon = document.querySelector(".user");
 
-  userIcon.addEventListener('keydown', function (evt) {
+  userIcon.addEventListener("keydown", function(evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
-      popup.classList.remove('hidden');
+      popup.classList.remove("hidden");
     }
   });
 })();
 
-Б22. Код является кроссбраузерным и не вызывает ошибок в разных браузерах и разных операционных системах
-При проверке этого критерия, необходимо удостовериться в правильной работе и отсутствии сообщений об ошибках в выполняемых скриптах только в последних версиях браузеров: Chrome, Firefox, Safari, Microsoft Edge.
+// Б22. Код является кроссбраузерным и не вызывает ошибок в разных браузерах и разных операционных системах
+// При проверке этого критерия, необходимо удостовериться в правильной работе и отсутствии сообщений об ошибках в выполняемых скриптах только в последних версиях браузеров: Chrome, Firefox, Safari, Microsoft Edge.
 
-Б23. Нельзя пользоваться глобальной переменной event
-Приводит к неосознанному коду:
+// Б23. Нельзя пользоваться глобальной переменной event
+// Приводит к неосознанному коду:
 
-var elem = document.querySelector('.test');
+var elem = document.querySelector(".test");
 
-var onElemClick = function () {
-  event.target.innerText = 'you really need event';
+var onElemClick = function() {
+  event.target.innerText = "you really need event";
 };
 
-elem.addEventListener('click', oneElemClick);
+elem.addEventListener("click", oneElemClick);
 
+// Б24. Своевременный выход из цикла: цикл не работает дольше чем нужно
+// Неправильно:
+
+apartments.forEach(function(it, index) {
+  if (index < 3) {
+    render(it);
+  }
+});
+
+// Правильно:
+
+for (var i = 0; i < Math.min(apartments.length, 3); i++) {
+  render(apartments[i]);
+}
