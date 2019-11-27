bf6b7567ea478ec7b893bcdd67bb685c1deeefd0
Den <fabri.din@yandex.ru>
2019-11-27 15:55:33 +0500

    add function

---
+++ events/gallery/script.js
@@ -0,0 +1,11 @@
+var photos = [
+  "gallery/laptop-large.jpg",
+  "gallery/microphone-large.jpg",
+  "gallery/keyboard-large.jpg",
+  "gallery/signboard-large.jpg",
+  "gallery/tree-large.jpg"
+];
+
+var thumbnails = document.querySelectorAll(".gallery__photo-preview");
+
+var fullPhoto = document.querySelector(".full-photo");
