2941075586f1cf39e30a3526fac8b9d7e393ead1
Den <fabri.din@yandex.ru>
2019-11-28 15:54:14 +0500

    add events

--- events/gallery/script.js
+++ events/gallery/script.js
@@ -9,3 +9,44 @@ var photos = [
 var thumbnails = document.querySelectorAll(".gallery__photo-preview");
 
 var fullPhoto = document.querySelector(".full-photo");
+//-----------------------------------------------------------
+
+var photos = [
+  "gallery/laptop-large.jpg",
+  "gallery/microphone-large.jpg",
+  "gallery/keyboard-large.jpg",
+  "gallery/signboard-large.jpg",
+  "gallery/tree-large.jpg"
+];
+
+var thumbnails = document.querySelectorAll(".gallery__photo-preview");
+var fullPhoto = document.querySelector(".full-photo");
+
+for (var i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener("click", function() {
+    console.log("Клик по картинке");
+  });
+}
+
+//---------------------------------------------------------
+
+var photos = [
+  "gallery/laptop-large.jpg",
+  "gallery/microphone-large.jpg",
+  "gallery/keyboard-large.jpg",
+  "gallery/signboard-large.jpg",
+  "gallery/tree-large.jpg"
+];
+
+var thumbnails = document.querySelectorAll(".gallery__photo-preview");
+var fullPhoto = document.querySelector(".full-photo");
+
+var addThumbnailClickHandler = function(thumbnail, photo) {
+  thumbnail.addEventListener("click", function() {
+    fullPhoto.src = photo;
+  });
+};
+
+for (var i = 0; i < thumbnails.length; i++) {
+  addThumbnailClickHandler(thumbnails[i], photos[i]);
+}
